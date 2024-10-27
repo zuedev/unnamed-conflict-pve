904ea8c6737090355ec3a01d3b825a4004b59316
zuedev <zuedev@gmail.com>
2024-10-27 06:15:22 +0000

    remove samples

--- samples/SampleMod_ModdedWeapon/Scripts/GameModded/Weapon/Modded/magazineWell.c
+++
@@ -1,8 +0,0 @@
-
-//AK74_Modded - 14.5x114mm
-class MagazineWellAK74_ModdedClass {}
-MagazineWellAK74_Modded MagazineWellAK74_ModdedSource;
-
-class MagazineWellAK74_Modded : BaseMagazineWell
-{
-}
--- samples/SampleMod_NewFaction/Scripts/Game/Editor/Enums/Modded/EEditableEntityLabel_SampleModNewFaction.c
+++
@@ -1,6 +0,0 @@
-modded enum EEditableEntityLabel
-{
-	FACTION_REDFOR = 14,
-	FACTION_BLUFOR = 15,
-	FACTION_GREENFOR = 16,
-}
